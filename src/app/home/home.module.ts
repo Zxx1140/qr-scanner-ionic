5047294e03caf61460f44d4b364a8ade1bfbaa6a
Zxx1140 <zixzax4545@gmail.com>
2023-07-06 04:53:02 +0100

    first qr

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-
+import { QRCodeModule } from 'angularx-qrcode';
 import { HomePageRoutingModule } from './home-routing.module';
 
 
@@ -12,8 +12,9 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    QRCodeModule
   ],
   declarations: [HomePage]
 })
-export class HomePageModule {}
+export class HomePageModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,75 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy } from '@angular/core';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit, OnDestroy {
 
-  constructor() {}
+  qrCodeString = 'This is a secret qr code message';
+  scannedResult: any;
+
+
+
+  constructor() { }
+  ngAfterViewInit(): void {
+
+  }
+
+  async checkPermission() {
+    try {
+      // check or request permission
+      const status = await BarcodeScanner.checkPermission({ force: true });
+      if (status.granted) {
+        // the user granted permission
+        return true;
+      }
+      return false;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+  }
+
+  async startScan() {
+    try {
+      const permission = await this.checkPermission();
+      if (!permission) {
+        return;
+      }
+
+      document.body.classList.add("qrscanner");
+
+
+      const result = await BarcodeScanner.startScan();
+      console.log(result);
+      BarcodeScanner.showBackground();
+      document.body.classList.remove("qrscanner");
+
+
+
+      if (result?.hasContent) {
+        this.scannedResult = result.content;
+        console.log(this.scannedResult);
+      }
+    } catch (e) {
+      console.log(e);
+      this.stopScan();
+    }
+  }
+
+  stopScan() {
+    document.body.classList.remove("qrscanner");
+    BarcodeScanner.showBackground();
+    BarcodeScanner.stopScan();
+
+  }
+
+  ngOnDestroy(): void {
+    this.stopScan();
+  }
+
 
 }
